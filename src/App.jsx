584eb84a446a365348bed70f22fc3bb355d4a017
Anurag Shukla <anurag090697@gmail.com>
2024-06-14 23:42:09 +0530

    only local storage needs to be included

--- src/App.jsx
+++ src/App.jsx
@@ -7,54 +7,79 @@ import Note from "./Note.jsx";
 import { IoMdAddCircle } from "react-icons/io";
 
 function App() {
-  const [note, setNote] = useState("");
+  // const [note, setNote] = useState("");
   const [notesArr, setNotesArr] = useState([
     {
-      title: "Enter Title Here",
-      content: "Enter Content Here",
+      title: "# Enter Title Here",
+      content: "** Enter Title here **",
     },
   ]);
 
-  function addNote() {
+  const [nowEditing, setNowEditing] = useState(0);
+  const changeCurrent = (index) => {
+    setNowEditing(index);
+  };
+  const modifyCurrentNote = (str) => {
+    let tm = [...notesArr];
+    tm[nowEditing].content = str;
+    tm[nowEditing].title = str.split("\n")[0];
+    setNotesArr(tm);
+  };
+
+  const addNote = () => {
+    // console.log("hiii");
     setNotesArr([
       ...notesArr,
       {
         title: "# Enter Title Here",
-        content: "Enter content here",
+        content: "** Enter Title here **",
       },
     ]);
-  }
+  };
 
-  function deleteNote(idx) {
+  const deleteNote = (idx) => {
+    console.log("hii");
     let tm = [...notesArr];
-    tm.slice(idx, 1);
+    tm.splice(idx, 1);
     setNotesArr(tm);
-  }
+  };
 
   return (
-    <div className='container min-h-100dvh'>
-      <section className='flex'>
-        <div className="w-72">
-          <div>
-            <h1>Notes</h1>
-            <button>
+    <div className='container bg-gray-700'>
+      <section className='flex h-dvh gap-2'>
+        <div className='w-96 bg-blue-200 flex flex-col gap-4 items-center '>
+          <div className='flex justify-around items-end p-2 w-full'>
+            <h1 className='text-4xl font-medium'>Notes</h1>
+            <button
+              className='text-3xl text-green-600 hover:text-green-500'
+              onClick={addNote}
+            >
               <IoMdAddCircle />
             </button>
-          </div>{" "}
-          <div>
-            {notesArr.map((ele, index) => {
-              <Note
-                data={ele}
-                key={index}
-                index={index}
-                deleteNote={deleteNote}
-              />;
-            })}
+          </div>
+          <div className='w-full px-4'>
+            {notesArr.map((ele, index) => (
+              <>
+                <Note
+                  data={ele}
+                  key={index}
+                  index={index}
+                  deleteNote={deleteNote}
+                  changeCurrent={changeCurrent}
+                />
+              </>
+            ))}
           </div>
         </div>
 
-        <div>
-          <MDEditor onChange={setNote} value={note} height="100%"/>
+        <div className='grow '>
+          <MDEditor
+            onChange={(value) => modifyCurrentNote(value)}
+            value={
+              // notesArr[nowEditing].title + "\n" +
+              notesArr[nowEditing].content}
+            height='100%'
+          />
         </div>
       </section>
     </div>
--- src/Note.jsx
+++ src/Note.jsx
@@ -4,15 +4,21 @@ import { FaRegEdit } from "react-icons/fa";
 
 function Note(props) {
   return (
-    <div>
-      <h2>{props.data.title}</h2>
-      <button>
-        <MdDelete />
-      </button>
-      <button>
-        <FaRegEdit />
-      </button>
-    </div>
+    <>
+      <div className='w-full m-auto overflow-hidden flex items-center gap-3'>
+        <h2 className='text-3xl'>{props.data.title}</h2>
+        <button onClick={() => props.deleteNote(props.index)}>
+          <MdDelete />
+        </button>
+        <button
+          onClick={() => props.changeCurrent(props.index)}
+          className='text-2xl text-'
+        >
+          <FaRegEdit />
+        </button>
+      </div>
+      <hr />
+    </>
   );
 }
 
